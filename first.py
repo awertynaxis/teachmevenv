0c1d5ed98af543a276327c844f7b4c81393bde56
awerty_naxis <awertynaxis@gmail.com>
2021-03-31 23:18:32 +0300

    Testing push

--- first.py
+++ first.py
@@ -1,8 +1,14 @@
-gor=False
-goro=False
-lol=[1,'hello',3,goro,gor,4,5]
-user=('Anton','Semenenko',375293685938)
-print(user)
-print('Hello world')
-print('Hello Roma')
-print('ssds')
+#Task 10
+# Дана строка целых чисел `numbers`, разделенных запятой.
+# Превратите строку в словарь `result`, где ключ словаря - число в строке, значение - число в кубе.
+# По возможности сделайте несколько вариантов решений, используя:
+# - цикл for (опционально while)
+# - словарное выражение (dict comprehension)
+numbers = '1,100,15,17,3,221,9,5,7,2,8,11'
+result = {}
+
+# Ваше решение
+
+# Тесты
+assert all(number in result and result[number] == number * number * number for number in (1, 100, 15, 17, 3, 221, 9, 5, 7, 2, 8, 11))
+```
