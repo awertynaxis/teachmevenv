800661ebcd647b6103fcbe6aebde57a1da637f59
awerty_naxis <awertynaxis@gmail.com>
2021-03-31 23:52:08 +0300

    Task 10 версия через цикл for

--- first.py
+++ first.py
@@ -1,14 +1,16 @@
 #Task 10
-# Дана строка целых чисел `numbers`, разделенных запятой.
-# Превратите строку в словарь `result`, где ключ словаря - число в строке, значение - число в кубе.
-# По возможности сделайте несколько вариантов решений, используя:
-# - цикл for (опционально while)
-# - словарное выражение (dict comprehension)
 numbers = '1,100,15,17,3,221,9,5,7,2,8,11'
 result = {}
-f
+d=''
+for x in numbers:
+    if x !=',':
+        d+=x
+    if x==',':
+        result[int(d)]=int(d)**3
+        d=''
+result[int(d)]=int(d)**3
+print(result)
 # Ваше решение
 
 # Тесты
 assert all(number in result and result[number] == number * number * number for number in (1, 100, 15, 17, 3, 221, 9, 5, 7, 2, 8, 11))
-```
